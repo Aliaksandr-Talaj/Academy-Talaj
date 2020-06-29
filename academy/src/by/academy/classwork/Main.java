dcfe3c928dde2f8c820b7fcd6e1fa74d3f95ee22
Aliaksandr-Talaj <booster.by@gmail.com>
2020-06-29 20:12:55 +0300

    123

--- academy/src/by/academy/classwork/Main.java
+++ academy/src/by/academy/classwork/Main.java
@@ -3,7 +3,7 @@ package by.academy.classwork;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Second commit!");
+		System.out.println("Hello World!");
 
 	}    
 
