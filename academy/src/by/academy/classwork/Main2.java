22d903000153fd35b690b3ebd1b7890d98442a50
Aliaksandr-Talaj <booster.by@gmail.com>
2020-06-29 19:48:33 +0300

    123

---
+++ academy/src/by/academy/classwork/Main2.java
@@ -0,0 +1,14 @@
+package by.academy.classwork;
+
+public class Main2 {
+	public static void main(String[] args) {
+		int myAge=24;
+		long passwordNumber=123456789L;
+		char yourMark='A';
+		double averageMark=4.12;
+		short test=512;
+		System.out.println("your exam results: ");
+		System.out.println("your age is: "+myAge+", passport number: "+passportNumber);
+		System.out.println("you got ");	
+	}
+}
