b0637815621a82895f1521f635988bd2da2e7e7f
Aliaksandr-Talaj <booster.by@gmail.com>
2020-06-29 20:17:14 +0300

    123

--- academy/src/by/academy/classwork/Main2.java
+++ academy/src/by/academy/classwork/Main2.java
@@ -3,12 +3,13 @@ package by.academy.classwork;
 public class Main2 {
 	public static void main(String[] args) {
 		int myAge=24;
-		long passwordNumber=123456789L;
+		long passportNumber=123456789L;
 		char yourMark='A';
 		double averageMark=4.12;
 		short test=512;
 		System.out.println("your exam results: ");
 		System.out.println("your age is: "+myAge+", passport number: "+passportNumber);
-		System.out.println("you got ");	
+		System.out.println("you got "+yourMark);
+		System.out.println("your average mark is "+averageMark);
 	}
 }
