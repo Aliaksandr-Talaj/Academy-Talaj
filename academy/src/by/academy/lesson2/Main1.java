af0429a45c1f235c43bfa56a2e9044e11b23bae1
Aliaksandr-Talaj <booster.by@gmail.com>
2020-06-30 23:53:27 +0300

    123

---
+++ academy/src/by/academy/lesson2/Main1.java
@@ -0,0 +1,12 @@
+package by.academy.lesson2;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int a=2123000;
+		byte b=100;
+		short c=234;
+		System.out.println(a/b*c);
+	}
+	
+}
