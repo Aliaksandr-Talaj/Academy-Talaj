e62b2ebbfcc1c068fbcd27a63b6b254bdf4552c9
Aliaksandr-Talaj <booster.by@gmail.com>
2020-07-01 13:55:43 +0300

    : New Task
    Task-Url:

--- academy/src/by/academy/lesson2/Main3.java
+++ academy/src/by/academy/lesson2/Main3.java
@@ -1,15 +1,11 @@
 package by.academy.lesson2;
 
-import java.util.Scanner;
-
 public class Main3 {
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int myInt=sc.nextInt();
-		sc.close();
-		if (myInt%2==0) {
-			System.out.println("Число "+ myInt +" является четным");
-		} else { System.out.println("Число "+ myInt +" является нечетным");
+		int i=(int)(Math.random()*100);
+		if (i%2==0) {
+			System.out.println("Число "+ i +" является четным");
+		} else { System.out.println("Число "+ i +" является нечетным");
 	}
 }
 }
