be8575ea5912e5c932589c8501f64fdaa8a41bee
Aliaksandr-Talaj <booster.by@gmail.com>
2020-07-15 21:21:46 +0300

    123

---
+++ academy/src/by/academy/classwork/lesson6/strings/task1/Main.java
@@ -0,0 +1,18 @@
+package by.academy.classwork.lesson6.strings.task1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Количество строк: ");
+		int n = sc.nextInt();
+		String [] strings = new String();
+		for (int i = 0; i < n; i++) {
+			
+		}
+		sc.close();
+	}
+
+}
