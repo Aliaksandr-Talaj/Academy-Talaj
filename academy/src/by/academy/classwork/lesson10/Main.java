cb60cbb49d8b58ceaaaba425808d98b937007554
Aliaksandr-Talaj <booster.by@gmail.com>
2020-07-27 21:23:31 +0300

    generics

---
+++ academy/src/by/academy/classwork/lesson10/Main.java
@@ -0,0 +1,20 @@
+package by.academy.classwork.lesson10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer [] arr1 = { 1, 2, 3, 4, 5 };
+		Double [] arr2 = { 1.1, 2.2, 3.3, 4.4, 5.0 };
+		
+		MinMax <Integer> mm1 = new MinMax<Integer>(arr1);
+		MinMax <Double> mm2 = new MinMax<Double>(arr2);
+		
+		System.out.println("Минимум массива 1: "+mm1.min());
+		System.out.println("Минимум массива 2: "+mm2.min());
+		System.out.println("Максимум массива 1: "+mm1.max());
+		System.out.println("Максимум массива 2: "+mm2.max());
+		mm1.bigger(mm2);
+		
+	}
+
+}
---
+++ academy/src/by/academy/classwork/lesson10/MinMax.java
@@ -0,0 +1,66 @@
+package by.academy.classwork.lesson10;
+
+public class MinMax<T extends Number> {
+
+	private T[] array;
+
+	public MinMax(T[] array) {
+		this.array = array.clone();
+	}
+
+	public double min() {
+		double minimal;
+		if (array.length != 0) {
+			minimal = array[0].doubleValue();
+
+			for (T value : array) {
+				if (minimal > value.byteValue()) {
+					minimal = value.doubleValue();
+				}
+			}
+		} else {
+			System.out.println("Массив пуст");
+			return 0;
+		}
+		return minimal;
+	}
+
+	public double max() {
+		double maximal;
+		if (array.length != 0) {
+			maximal = array[0].doubleValue();
+
+			for (T value : array) {
+				if (maximal < value.doubleValue()) {
+					maximal = value.doubleValue();
+				}
+			}
+		} else {
+			System.out.println("Массив пуст");
+			return 0;
+		}
+		return maximal;
+	}
+
+	public void bigger(MinMax<?> mm2) {
+		if (max() > mm2.max()) {
+			System.out.println("Максимум первого массива больше:\n" + max() + ">" + mm2.max());
+		} else {
+			if (max() < mm2.max()) {
+				System.out.println("Максимум второго массива больше:\n" + max() + "<" + mm2.max());
+			} else {
+				System.out.println("Максимумы массивов равны:\n" + max() + "=" + mm2.max());
+			}
+		}
+		if (min() > mm2.min()) {
+			System.out.println("Минимум первого массива больше:\n" + min() + ">" + mm2.min());
+		} else {
+			if (min() < mm2.min()) {
+				System.out.println("Минимум второго массива больше:\n" + min() + "<" + mm2.min());
+			} else {
+				System.out.println("Минимумы массивов равны:\n" + min() + "=" + mm2.min());
+			}
+		}
+	}
+
+}
