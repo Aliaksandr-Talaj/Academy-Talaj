27219baaecb9ab4a780dcec78577776000344d6a
Aliaksandr-Talaj <booster.by@gmail.com>
2020-07-29 21:22:50 +0300

    Classwork

--- academy/src/by/academy/classwork/collections/Task12.java
+++ academy/src/by/academy/classwork/collections/Task12.java
@@ -16,11 +16,27 @@ public class Task12 {
 		for (int i=0;i<100_000;i++) {
 			arrayList.add(0,Math.random()*10);
 		}
-		System.out.println("Время выполнения ArrayList, мс: " + (System.currentTimeMillis() - start));
+		System.out.println("Время добавления в ArrayList, мс: " + (System.currentTimeMillis() - start));
 		start = System.currentTimeMillis();
 		for (int i=0;i<100_000;i++) {
 			linkedList.add(0,Math.random()*10);
 		}
-		System.out.println("Время выполнения LinkedList, мс: " + (System.currentTimeMillis() - start));
+		System.out.println("Время добавления в LinkedList, мс: " + (System.currentTimeMillis() - start));
+	
+		
+		start = System.currentTimeMillis();
+		
+		for (int i=0;i<100_000;i++) {
+			arrayList.get(i);
+		}
+		System.out.println("Время взятия из ArrayList, мс: " + (System.currentTimeMillis() - start));
+		start = System.currentTimeMillis();
+	
+		start = System.currentTimeMillis();
+		for (int i=0;i<100_000;i++) {
+			linkedList.get(i);
+		}
+		System.out.println("Время взятия из LinkedList, мс: " + (System.currentTimeMillis() - start));
+		
 	}
 }
